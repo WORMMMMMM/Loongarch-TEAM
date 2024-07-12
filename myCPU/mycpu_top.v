0dc3364ccbe6a709afe120ac2fc0326ef728a4f0
ONLOX <3316820553@qq.com>
2024-07-12 17:03:29 +0800

    feat(myCPU): create file structure

---
+++ myCPU/mycpu_top.v
@@ -0,0 +1,26 @@
+module mycpu_top(
+    input         clk,
+    input         resetn,
+    // inst sram interface
+    output        inst_sram_en,
+    output [ 3:0] inst_sram_wen,
+    output [31:0] inst_sram_addr,
+    output [31:0] inst_sram_wdata,
+    input  [31:0] inst_sram_rdata,
+    // data sram interface
+    output        data_sram_en,
+    output [ 3:0] data_sram_wen,
+    output [31:0] data_sram_addr,
+    output [31:0] data_sram_wdata,
+    input  [31:0] data_sram_rdata,
+    // trace debug interface
+    output [31:0] debug_wb_pc,
+    output [ 3:0] debug_wb_rf_wen,
+    output [ 4:0] debug_wb_rf_wnum,
+    output [31:0] debug_wb_rf_wdata
+);
+reg         reset;
+always @(posedge clk) reset <= ~resetn;
+
+
+endmodule
