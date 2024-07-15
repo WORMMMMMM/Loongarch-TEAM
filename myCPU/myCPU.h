8e8af6502a40bd142eb6e53e70308bbdac3c1ad0
ONLOX <3316820553@qq.com>
2024-07-15 23:51:31 +0800

    refactor: :ambulance: use openla500 as the baseline

--- myCPU/myCPU.h
+++ myCPU/myCPU.h
@@ -1,15 +1,23 @@
-`ifndef myCPU_H
-    `define myCPU_H
-    /* Bus width */
-    `define FS_TO_DS_BUS_WD 64
-    `define DS_TO_ES_BUS_WD 167
-    `define ES_TO_MS_BUS_WD 211
-    `define MS_TO_WS_BUS_WD 199
-    `define WS_TO_FS_BUS_WD 261
-    `define BR_BUS_WD 32
-    `define WB_BUS_WD 38
-    // `define ES_FORWARD_WD   59
-    // `define MS_FORWARD_WD   60
-    // `define WS_FORWARD_WD   124
-    // `define WS_TO_ES_BUS_WD 160
+`ifndef MYCPU_H
+    `define MYCPU_H
+
+//    `define BR_BUS_WD       33  //bug5 32->33
+//    `define FS_TO_DS_BUS_WD 109
+//    `define DS_TO_ES_BUS_WD 236
+//    `define ES_TO_MS_BUS_WD 215
+//    `define MS_TO_WS_BUS_WD 218
+//    `define WS_TO_RF_BUS_WD 38
+//    `define ES_TO_DS_FORWARD_BUS 39
+//    `define MS_TO_DS_FORWARD_BUS 39
+
+    `define BR_BUS_WD       33  //bug5 32->33
+    `define FS_TO_DS_BUS_WD 109
+    `define DS_TO_ES_BUS_WD 350
+    `define ES_TO_MS_BUS_WD 425
+    `define MS_TO_WS_BUS_WD 460
+    `define WS_TO_RF_BUS_WD 38
+    `define ES_TO_DS_FORWARD_BUS 39
+    `define MS_TO_DS_FORWARD_BUS 39
 `endif
+
+//`define SIMU
