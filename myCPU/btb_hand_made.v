3f56ff198f87e02c4184d7295d6a8def19c70199
grange <2634070476@qq.com>
2024-08-11 23:29:13 +0800

    ✨  try straight use openla btb

--- myCPU/btb_hand_made.v
+++ myCPU/btb_hand_made.v
@@ -36,6 +36,7 @@ reg [BTBNUM-1:0] btb_valid;
 
 reg [29:0]  ras          [7:0];
 reg [ 3:0]  ras_ptr;
+wire [29:0] ras_top;
 
 reg [29:0]  ras_pc       [RASNUM-1:0];
 reg [RASNUM-1:0] ras_valid;
@@ -48,6 +49,28 @@ wire [15:0] ras_match_rd;
 wire [4:0]  btb_match_index;
 wire [3:0]  ras_match_index;
 
+wire [29:0] btb_match_target;
+wire [1:0] btb_match_counter;
+
+wire btb_all_entry_valid;
+wire [4:0] btb_select_one_invalid_entry;
+wire [4:0] btb_add_entry_index;
+reg  [4:0] btb_delete_entry_index;
+wire [31:0] btb_add_entry_dec;
+
+wire ras_all_entry_valid;
+wire [3:0] ras_select_one_invalid_entry;
+wire [3:0] ras_add_entry_index;
+
+wire [31:0] btb_untaken_entry;
+reg  [31:0] btb_untaken_entry_r;
+wire [31:0] btb_untaken_entry_t;
+reg  btb_add_entry_r;
+wire [4:0] btb_select_one_untaken_entry;
+wire btb_has_one_untaken_entry;
+
+
+
 always @(posedge clk) begin
     if (reset)
         fetch_en_r <= 1'b0;
@@ -56,6 +79,11 @@ always @(posedge clk) begin
 
     if (fetch_en)
         fetch_pc_r <= fetch_pc;
+end
+
+always @(posedge clk) begin
+    btb_untaken_entry_r <= btb_untaken_entry;
+    btb_add_entry_r <= operate_en && add_entry && !pop_ras;    
 end
 
 genvar i;
@@ -85,8 +113,16 @@ encoder_16_4 enc_ras_match_rd(
     .out(ras_match_index)
 );
     
-assign ret_en = (|btb_match_rd) || (|ras_match_rd);
+assign btb_match_target = btb_target[btb_match_index];
+assign btb_match_counter = btb_counter[btb_match_index];
 
+assign ret_en = (|btb_match_rd) || (|ras_match_rd);
 assign ret_pc = (|btb_match_rd) ? {btb_target[btb_match_index], 2'b0} : (|ras_match_rd) ? {ras_top, 2'b0} : 32'b0;
+
+assign taken = btb_match && btb_match_counter[1] || ras_match;
+assign ret_index = (|btb_match_rd) ? btb_match_index : (|ras_match_rd) ? {1'b0, ras_match_index} : 5'b0;
+
+assign res_full = (ras_ptr[3] == 1'b1);
+assign res_empty = (ras_ptr == 4'b0);
 
 endmodule
